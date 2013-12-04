6549ba8659a1672f86130bdc82b3b0aa0b72bbca
tonyp <tonyptonyp@gmail.com>
2013-12-04 04:14:49 +0100

    Revert "SurfaceFlinger: Dont show Animation on External Display"
    This reverts commit e242f0dc69009d58e65727b03887774709649486.
    
    Conflicts:
    	services/surfaceflinger/DisplayHardware/HWComposer.cpp

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -100,8 +100,7 @@ public:
     void run() ANDROID_API;
 
     enum {
-        EVENT_VSYNC = HWC_EVENT_VSYNC,
-        EVENT_ORIENTATION = HWC_EVENT_ORIENTATION
+        EVENT_VSYNC = HWC_EVENT_VSYNC
     };
 
     // post an asynchronous message to the main thread
